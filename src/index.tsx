930cb988cc5612031a31af587cc73271e2d68112
chau-nm <chaunm.146.2001@gmail.com>
2024-04-20 16:49:13 +0700

    fix bug eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.scss';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
